6df25b2dce6f64acd735004c09dec89b26f51311
lxmd01 <74915354+lxmd01@users.noreply.github.com>
2020-11-28 13:39:14 +0800

    Update qqread.js

--- qqread.js
+++ qqread.js
@@ -31,11 +31,13 @@ const wktimess=1200//周奖励领取标准，默认1200分钟
 
 const qqreadurlVal = 'https://mqqapi.reader.qq.com/mqq/user/init'
 
-let qqreadheaderVal = '',qqreadtimeurlVal = '',qqreadtimeheaderVal = '';
+let qqreadheaderVal = "",qqreadtimeurlVal = "",qqreadtimeheaderVal = "";
 
 	qqreadheaderVal= process.env.QQREAD_HEADER
 	qqreadtimeurlVal = process.env.QQREAD_TIMEBODY
 	qqreadtimeheaderVal = process.env.QQREAD_TIMEHEADER
+var qqreadheaderVal
+
 
 var tz=''
 

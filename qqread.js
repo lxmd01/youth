6b0b8e5332a7a8ab3483c82d785290c391cb8aa3
lxmd01 <74915354+lxmd01@users.noreply.github.com>
2020-11-28 13:15:43 +0800

    Update qqread.js

--- qqread.js
+++ qqread.js
@@ -30,11 +30,15 @@ const maxtime=20//每日上传时长限制，默认20小时
 const wktimess=1200//周奖励领取标准，默认1200分钟
 
 const qqreadurlVal = 'https://mqqapi.reader.qq.com/mqq/user/init'
+let ReadArr = [], YouthBody = "",readscore = [];
 
-	qqreadheaderVal = process.env.QQREAD_HEADER
-	qqreadtimeurlVal = process.env.QQREAD_TIMEBODY
-	qqreadtimeheaderVal = process.env.QQREAD_TIMEHEADER
-
+	ReadArr = process.env.QQREAD_HEADER
+	YouthBody = process.env.QQREAD_TIMEBODY
+	readscore = process.env.QQREAD_TIMEHEADER
+        qqreadheaderVal = ReadArr
+        qqreadurlVal = YouthBody
+        qqreadtimeheaderVal = readscore
+        
 
 var tz=''
 

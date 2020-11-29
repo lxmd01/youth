e9649b5f85cf3eb0d2ee0de843101ce5d6f4d69a
lxmd01 <74915354+lxmd01@users.noreply.github.com>
2020-11-29 13:07:40 +0800

    Update qqread.js

--- qqread.js
+++ qqread.js
@@ -31,12 +31,10 @@ const wktimess=1200//周奖励领取标准，默认1200分钟
 
 const qqreadurlVal = 'https://mqqapi.reader.qq.com/mqq/user/init'
 const qqreadtimeurlVal = 'https://mqqapi.reader.qq.com/mqq/addReadTimeWithBid?scene=1132&refer=-1&bid=130833&readTime=2190&read_type=0&conttype=1&read_status=0&chapter_info=%5B%7B%221%22%3A%7B%22readTime%22%3A2190%2C%22pay_status%22%3A4%7D%7D%5D&sp=-1'
-let cookiesArr = [], qqreadheaderVal = '',
-    readArr = [], qqreadtimeheaderVal='',
+let cookiesArr = [], qqreadheaderVal,
+    readArr = [], qqreadtimeheaderVal;
 let CookieYouth = [] ,ARTBODYs = [], 
     REDBODYs  = [], READTIME = [];
-//const qqreadheaderVall= process.env.QQREAD_HEADER
-//const qqreadtimeheaderVall=process.env.QQREAD_TIMEHEADER
 if ($.isNode()) {
   if (process.env.QQREAD_HEADER && process.env.QQREAD_HEADER.indexOf('#') > -1) {
   CookieYouth = process.env.QQREAD_HEADER.split('#');

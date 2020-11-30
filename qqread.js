17aa37b5545995ee02c79432563c1278112d9b3a
lxmd01 <74915354+lxmd01@users.noreply.github.com>
2020-11-30 23:17:38 +0800

    Update qqread.js

--- qqread.js
+++ qqread.js
@@ -60,7 +60,7 @@ for (let index = 0; index < headers.length; index++) {
   cookiesArr.push(json_temp);
 }
 console.log(`============ 脚本执行-国际标准时间(UTC)：${new Date().toLocaleString()}  =============\n`)
-console.log(`============ 脚本执行-北京时间(UTC+8)：${new Date(new Date().getTime() + 12 * 60 * 60 * 1000).toLocaleString()}  =============\n`)
+console.log(`============ 脚本执行-北京时间(UTC+8)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}  =============\n`)
 
 var tz = "";
 let num = 0;

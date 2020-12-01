725f92aaf664bf01225766bc89f005d3e5f9e38e
lxmd01 <74915354+lxmd01@users.noreply.github.com>
2020-12-01 19:53:40 +0800

    Update qqread.js

--- qqread.js
+++ qqread.js
@@ -682,25 +682,25 @@ function showmsg() {
 console.log(tz)
 let d = new Date(new Date().getTime() + 8 * 60 * 60 * 1000);
 let gold=Number(task.data.user.amount)
-console.log(gold)
+
 if (notifyInterval==1&&gold >= 50000 && d.getHours()>=9 && d.getHours()<=20&&task.data.treasureBox.doneFlag==0)
 
 notify.sendNotify(jsname,tz,'')//显示所有通知
 	
-
+console.log('启用全部通知！')
 	
 else if (notifyInterval==2&&gold >= 50000&&d.getHours()>=9&&d.getHours()<=20&&task.data.treasureBox.doneFlag==0)
 
 	notify.sendNotify(jsname,tz,'')//宝箱领取成功通知
 	
-
+console.log('宝箱领取通知！')
 	
 else if (notifyInterval==3&&gold >= 50000&&d.getHours()>=9&&d.getHours()<=20&&task.data.treasureBox.doneFlag==0&&task.data.treasureBox.count==0||task.data.treasureBox.count==15||task.data.treasureBox.count==30||task.data.treasureBox.count==45||task.data.treasureBox.count==60)
 notify.sendNotify(jsname,tz,'')//宝箱每15次通知一次
-
+console.log('宝箱每15次通知一次！')
 else if (d.getHours()==19&&d.getMinutes()>=40)
 notify.sendNotify(jsname,tz,'')//每天19点40分通知一次	
-
+console.log('每天固定时间通知！')
 tz=''
 }
 

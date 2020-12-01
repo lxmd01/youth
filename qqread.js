813348197372133dcbbc5ba8a00f21f919de18dc
lxmd01 <74915354+lxmd01@users.noreply.github.com>
2020-12-01 19:32:10 +0800

    Update qqread.js

--- qqread.js
+++ qqread.js
@@ -687,20 +687,20 @@ if (notifyInterval==1&&gold >= 50000 && d.getHours()>=9 && d.getHours()<=20&&tas
 
 notify.sendNotify(jsname,tz,'')//显示所有通知
 	
-console.log("所有通知")
+console.log('所有通知')
 	
 else if (notifyInterval==2&&gold >= 50000&&d.getHours()>=9&&d.getHours()<=20&&task.data.treasureBox.doneFlag==0)
 
 	notify.sendNotify(jsname,tz,'')//宝箱领取成功通知
 	
-console.log("宝箱领取成功通知")
+console.log('宝箱领取成功通知')
 	
 else if (notifyInterval==3&&gold >= 50000&&d.getHours()>=9&&d.getHours()<=20&&task.data.treasureBox.doneFlag==0&&task.data.treasureBox.count==0||task.data.treasureBox.count==15||task.data.treasureBox.count==30||task.data.treasureBox.count==45||task.data.treasureBox.count==60)
 notify.sendNotify(jsname,tz,'')//宝箱每15次通知一次
-	console.log("宝箱每15次通知一次")
+	console.log('宝箱每15次通知一次')
 else if (d.getHours()==19&&d.getMinutes()>=40)
 notify.sendNotify(jsname,tz,'')//每天19点40分通知一次	
-	 console.log("固定通知")
+	 console.log('固定通知')
 tz=''
 }
 
